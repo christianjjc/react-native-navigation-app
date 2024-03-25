083017ed27dbfbdcc928b13bce739212f554e23e
christianjjc <christianjjc@gmail.com>
2024-03-25 12:11:59 -0500

    StackNavigation temrinado

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import {View} from 'react-native';
 import {globalStyles} from '../../theme/theme';
 import {PrimaryButton} from '../../components/shared/PrimaryButton';
-import {useNavigation} from '@react-navigation/native';
+import {type NavigationProp, useNavigation} from '@react-navigation/native';
+import {type RootStackParams} from '../../routes/StackNavigator';
 
 export const HomeScreen = () => {
-  const navigation = useNavigation();
+  const navigation = useNavigation<NavigationProp<RootStackParams>>();
 
   return (
     <View style={globalStyles.container}>
       <PrimaryButton
         label="Products"
-        onPress={() => navigation.navigate('Products' as never)}
+        onPress={() => navigation.navigate('Products')}
       />
       <PrimaryButton
         label="Settings"
-        onPress={() => navigation.navigate('Settings' as never)}
+        onPress={() => navigation.navigate('Settings')}
       />
     </View>
   );
--- src/presentation/screens/settings/Settings.Screen.tsx
+++ src/presentation/screens/settings/Settings.Screen.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import {Text, View} from 'react-native';
+import {PrimaryButton} from '../../components/shared/PrimaryButton';
+import {globalStyles} from '../../theme/theme';
+import {StackActions, useNavigation} from '@react-navigation/native';
 
 export const SettingsScreen = () => {
+  const navigator = useNavigation();
+
   return (
-    <View>
-      <Text>SettingsScreen.tsx</Text>
+    <View style={globalStyles.container}>
+      <Text style={{marginBottom: 10}}>Settings Screen</Text>
+      <PrimaryButton label="Regresar" onPress={() => navigator.goBack()} />
+      <PrimaryButton
+        label="Regresar al Home"
+        onPress={() => navigator.dispatch(StackActions.popToTop())}
+      />
     </View>
   );
 };
