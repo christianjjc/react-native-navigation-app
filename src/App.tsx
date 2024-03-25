494270c3aa42a9230913da55167890a4504b99d9
christianjjc <christianjjc@gmail.com>
2024-03-25 15:53:37 -0500

    fin app con 'Stack-Navigator-Drawer'  On branch Stack-Navigator-Drawer  Changes to be committed: 	modified:   babel.config.js 	modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx 	new file:   src/presentation/routes/SideMenuNavigator.tsx 	modified:   src/presentation/screens/home/HomeScreen.tsx 	modified:   src/presentation/screens/profile/ProfileScreen.tsx 	modified:   yarn.lock

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {StackNavigator} from './presentation/routes/StackNavigator';
+import {SideMenuNavigator} from './presentation/routes/SideMenuNavigator';
 
 export const App = () => {
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <SideMenuNavigator />
     </NavigationContainer>
   );
 };
